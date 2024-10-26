949a272655382fd45042d0b41768b40aab0c7c23
WajahatKanju <wajahat5ahmad@gmail.com>
2024-10-26 15:57:45 +0500

    Adjust theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,27 +1,22 @@
-// src/theme.ts
+// @ts-expect-error charaka UI Error for no reason - vscode issue
 import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   colors: {
-    primary: "#3498db",
-    secondary: "#2ecc71",
-    accent: "#e74c3c",
-    background: "#ecf0f1",
-    text: "#34495e",
+    primary: "#7E1FBA",
+    secondary: "#293056",
+    accent: "##9796F0",
+    background: "#ffffff",
+    text: "#293056",
   },
   fonts: {
-    heading: "Roboto, sans-serif",
-    body: "Open Sans, sans-serif",
+    heading: "Poppins, sans-serif",
+    body: "Poppins, sans-serif",
   },
   fontSizes: {
     h1: "32px",
     h2: "28px",
     body: "16px",
-  },
-  spacing: {
-    sm: "8px",
-    md: "16px",
-    lg: "32px",
   },
   styles: {
     global: {
