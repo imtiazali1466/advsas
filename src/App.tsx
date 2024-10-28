f1d0c4ac801f5040010249b49bb9b5c9bc8ffe3c
WajahatKanju <wajahat5ahmad@gmail.com>
2024-10-28 09:51:14 +0500

    update Technology useded

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { Box } from "@chakra-ui/react";
 import "./App.css";
 import "./i18n"; // Import the i18n configuration
-import Navbar from "./components/navbar/Navbar";
 import { useTranslation } from "react-i18next";
 
 const App: React.FC = () => {
