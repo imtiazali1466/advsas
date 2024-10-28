6b9f6e9778830d910f0aaadb5808966ce84d401c
WajahatKanju <wajahat5ahmad@gmail.com>
2024-10-28 11:51:05 +0500

    add webnavbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,27 +1,41 @@
-// src/components/navbar/Navbar.tsx
 import React from "react";
 import {
   Box,
+  Flex,
   HStack,
   Button,
   Menu,
   MenuButton,
   MenuList,
   MenuItem,
+  IconButton,
+  useDisclosure,
+  VStack,
+  Collapse,
+  Image,
+  useBreakpointValue,
 } from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import NavbarLink from "./NavbarLink";
 import { useTranslation } from "react-i18next";
 import { useScrollSpy } from "../../hooks/useScrollSpy";
+import { FaTwitter, FaInstagram, FaGithub } from "react-icons/fa";
+import theme from "@/theme";
 
-const sections = ["hero", "about", "services", "contact"];
+const sections = ["home", "about", "services", "projects", "blog"];
 
 const Navbar: React.FC = () => {
-  const { i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
   const activeSection = useScrollSpy(sections);
+  const { isOpen, onToggle } = useDisclosure();
+
+  // Set 'home' as the active section by default if no section is active
+  const currentActiveSection = activeSection || "home";
 
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
   };
+  const buttonPadding = useBreakpointValue({ base: 2, md: 3 });
 
   return (
     <Box
@@ -29,31 +43,124 @@ const Navbar: React.FC = () => {
       position="fixed"
       top={0}
       width="100%"
-      bg="white"
+      bg="white.900" // Dark background color
       py={4}
       zIndex="1000"
+      boxShadow="lg"
     >
-      <HStack spacing={8} justify="center">
-        {sections.map((section) => (
-          <NavbarLink
-            key={section}
-            sectionId={section}
-            isActive={activeSection === section}
-          >
-            {section.charAt(0).toUpperCase() + section.slice(1)}
-          </NavbarLink>
-        ))}
+      <Flex justify="space-between" align="center" px={6}>
+        {/* Logo */}
+        <Box>
+          <Image
+            src="/navbar_logo.png"
+            alt={t("navbar.logoAlt")}
+            borderRadius="full"
+          />
+        </Box>
 
-        <Menu>
-          <MenuButton as={Button}>
-            {i18n.language === "en" ? "English" : "Arabic"}
-          </MenuButton>
-          <MenuList>
-            <MenuItem onClick={() => changeLanguage("en")}>English</MenuItem>
-            <MenuItem onClick={() => changeLanguage("ar")}>Arabic</MenuItem>
-          </MenuList>
-        </Menu>
-      </HStack>
+        {/* Hamburger Icon for mobile */}
+        <IconButton
+          aria-label="Toggle Navigation"
+          icon={
+            isOpen ? (
+              <CloseIcon color="white" />
+            ) : (
+              <HamburgerIcon color="white" />
+            )
+          }
+          onClick={onToggle}
+          display={{ md: "none" }}
+          variant="ghost" // Transparent button for better aesthetics
+        />
+
+        {/* Navigation Links */}
+        <HStack
+          gap={8}
+          display={{ base: "none", md: "flex" }}
+          justify="center"
+          bg="navbar" // Adjust to match your theme
+          padding={4}
+          borderRadius={40}
+        >
+          {sections.map((section) => (
+            <NavbarLink
+              key={section}
+              sectionId={section}
+              isActive={currentActiveSection === section}
+            >
+              {t(`navbar.${section}`)}
+            </NavbarLink>
+          ))}
+          <Button
+            variant="link"
+            color={"text"} // Change to primary when active
+            padding={buttonPadding}
+            borderRadius={20}
+            fontWeight={"bold"}
+            borderWidth={"2px"}
+            background={theme.colors.white[100]}
+            borderColor={theme.colors.white[100]}
+            textTransform={"uppercase"}
+            _hover={{
+              borderColor: theme.colors.white[100], // Hover color from primary
+            }}
+          >
+            {t(`navbar.book`)}
+          </Button>
+        </HStack>
+
+        {/* Language Selection and Social Icons */}
+        <HStack spacing={4} justify="flex-end" mt={4} px={6}>
+          <Menu>
+            <MenuButton as={Button} variant="outline" colorScheme="teal">
+              {i18n.language === "en" ? "English" : "Arabic"}
+            </MenuButton>
+            <MenuList>
+              <MenuItem onClick={() => changeLanguage("en")}>English</MenuItem>
+              <MenuItem onClick={() => changeLanguage("ar")}>Arabic</MenuItem>
+            </MenuList>
+          </Menu>
+          <IconButton
+            as="a"
+            href="https://twitter.com"
+            aria-label="Twitter"
+            icon={<FaTwitter color="text" />}
+            target="_blank"
+            variant="link"
+          />
+          <IconButton
+            as="a"
+            href="https://instagram.com"
+            aria-label="Instagram"
+            icon={<FaInstagram color="text" />}
+            target="_blank"
+            variant="link"
+          />
+          <IconButton
+            as="a"
+            href="https://github.com"
+            aria-label="GitHub"
+            icon={<FaGithub color="text" />}
+            target="_blank"
+            variant="link"
+          />
+        </HStack>
+      </Flex>
+
+      {/* Mobile Navigation Links */}
+      <Collapse in={isOpen}>
+        <VStack spacing={4} align="start" mt={4} px={6}>
+          {sections.map((section) => (
+            <NavbarLink
+              key={section}
+              sectionId={section}
+              isActive={currentActiveSection === section}
+            >
+              {t(`navbar.${section}`)}
+            </NavbarLink>
+          ))}
+        </VStack>
+      </Collapse>
     </Box>
   );
 };
